a5389e4abb8db0001716dfd76edea2fa6e4b6740
isaacedwardtyler <isaacedwardtyler@gmail.com>
2024-05-13 12:40:23 +0100

    copy old website

---
+++ docs/js/scroll.js
@@ -0,0 +1,26 @@
+// Smooth scroll functionality
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(function (entry) {
+        if (!entry.isIntersecting) {
+            entry.target.classList.add('not-visible');
+        } else {
+            entry.target.classList.remove('not-visible');
+        }
+    });
+});
+document.querySelectorAll('.show-on-scroll').forEach(e => observer.observe(e));
+
+function toggleHamburgerMenu() {
+    let nav = document.getElementById("navbar");
+    let nav2 = document.getElementById("navbar-list");
+    if (nav.classList.contains("expanded")) {
+        nav.classList.remove("expanded-show-bg");
+        nav2.classList.remove("expanded-show-bg");
+        // allow transition to run
+        setTimeout(() => nav.classList.remove("expanded"), 400); 
+    } else {
+        nav.classList.add("expanded");
+        nav.classList.add("expanded-show-bg");
+        nav2.classList.add("expanded-show-bg");
+    }
+}
---
+++ docs/js/video.js
@@ -0,0 +1,76 @@
+const main = async () => {
+    let images = [];
+
+    // Check if browser is mobile or desktop
+    let file = "launch-horz.tar"
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        console.log("Using mobile launch images")
+    //    file = "launch-vert.tar"
+    //lets keep this simple
+    } else {
+        console.log("Using desktop launch images")
+    }
+
+    let resp = await fetch("assets/" + file);
+
+    // GitHub Pages doesn't send Content-Length so we have to hardcode it
+    let length = file == "launch-horz.tar" ? 4055040 : 1464320;
+
+    let loaded = 0
+
+    resp = new Response(new ReadableStream({
+        async start(controller) {
+            const reader = resp.body.getReader();
+
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) break;
+                loaded += value.byteLength;
+                document.getElementById("loading-bar").value = Math.floor(loaded / length * 100);
+                controller.enqueue(value);
+            }
+            controller.close();
+
+            document.getElementById("jumbotron-label").classList.remove("hidden");
+        }
+    }));
+
+    let files = await untar(await resp.arrayBuffer());
+    files.forEach((file) => {
+        let img = new Image();
+        img.src = file.getBlobUrl();
+        images.push(img);
+    });
+
+    let jumbotronFront = document.getElementById("jumbotron-content-back");
+    let jumbotronBack = document.getElementById("jumbotron-content");
+    let jumbo = document.getElementById("jumbotron-sticky")
+
+    let next = jumbotronFront;
+    let other = jumbotronBack;
+
+    let frame = () => {
+        let scrolled = document.body.scrollTop;
+        let jumboStart = jumbo.offsetTop;
+        let jumboEnd = jumbo.offsetTop + jumbo.offsetHeight;
+        let scrolledPastJumbo = (jumboStart - scrolled) / (jumboEnd - jumboStart);
+        let currentFrame = Math.floor(scrolledPastJumbo * (images.length - 1));
+        let img = images[currentFrame];
+        if (!img) {
+            // Wait for the image to load
+            requestAnimationFrame(frame);
+            return;
+        }
+        img.decode().then(() => {
+            next.style.backgroundImage = `url(${img.src})`;
+            other.style.zIndex = 100;
+            next.style.zIndex = 1000;
+        });
+        window.requestAnimationFrame(frame);
+        next = next == jumbotronFront ? jumbotronBack : jumbotronFront;
+        other = other == jumbotronFront ? jumbotronBack : jumbotronFront;
+    }
+    window.requestAnimationFrame(frame);
+}
+
+window.onload = main;
